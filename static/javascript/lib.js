6b4a75978c3ef5a1c9022dacab055c6c1a0c7329
Harrington Joseph <h.josephv@gmail.com>
2011-02-14 12:22:55 +0100

    Floating menu and scrolling efect

---
+++ static/javascript/lib.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        
+		$('div.navbar > ul.menu > li > a').click(function(){
+			return slideScroll($(this.hash));
+		})
+});
+
+function slideScroll(target){
+        var $target = target;
+        $target = $target.length && $target     || $('[name=' + this.hash.slice(1) +']');
+        var targetOffset = $target.offset().top - 90;
+        $('html,body').animate({scrollTop: targetOffset}, 1000);
+        return false;
+}
