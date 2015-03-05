2a2b98368f764b673b8f47ccdadd3bf93b7d8c62
Harrington Joseph <harrington.joseph@demandmedia.com>
2015-03-05 14:22:34 -0800

    created base and index template

--- src/views.py
+++ src/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from flask.views import MethodView
+from jinja2 import Environment, FileSystemLoader
 
 
 class IndexView(MethodView):
     def get(self):
-        return "get"
+        return render_template('index.html')
